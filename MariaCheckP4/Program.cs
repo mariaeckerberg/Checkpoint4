0938e9d67b2842cba939fe847da3df6a1087bc59
Maria Eckerberg <maria@eckerberg.se>
2018-11-02 15:51:04 +0100

    Add Best movies ever CW.

--- MariaCheckP4/Program.cs
+++ MariaCheckP4/Program.cs
@@ -7,6 +7,7 @@ namespace MariaCheckP4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!!!!!!");
+            Console.WriteLine("\nBest movies ever!");
         }
     }
 }
