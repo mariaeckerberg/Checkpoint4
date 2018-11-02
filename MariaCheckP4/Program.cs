7f3e067f1846593081399d85653b48844296c6fa
Maria Eckerberg <maria@eckerberg.se>
2018-11-02 15:43:47 +0100

    Add project files.

---
+++ MariaCheckP4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MariaCheckP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
